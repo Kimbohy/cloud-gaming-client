b935ca14fcab5a9822c85f59615920decea2e9d1
Henintsoaaa <andriamifidyhenintsoa@gmail.com>
2025-11-28 14:16:41 +0300

    feat: Implement authentication features with sign in and sign up functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import AuthPage from "./pages/AuthPage";
+import AuthPage from "./pages/AuthPage";
 import PlayPage from "./pages/PlayPage";
 import RomsPage from "./pages/RomsPage";
 import { NuqsAdapter } from "nuqs/adapters/react-router/v6";
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <main className="">
           <Routes>
-            {/* <Route path="/auth" element={<AuthPage />} /> */}
+            <Route path="/auth" element={<AuthPage />} />
             <Route path="/roms" element={<RomsPage />} />
             <Route path="/play/:id" element={<PlayPage />} />
             <Route path="*" element={<RomsPage />} />
